630515eaf6c31681a93ec20fe3fc3bb1c46b0e6b
Grigoriy Nakonechnyi <nakan88@gmail.com>
2016-06-21 23:29:58 +0300

    code review #3

--- js/main.js
+++ js/main.js
@@ -1,18 +1,18 @@
 $(document).ready(function () {
-	var name, number, time, rowId, grid;
+	var name, number, time, rowId, grid; // use should use only one variable;
 	var callCount = 0,
 		nameValid = false,
 		numberValid = false,
 		timeValid = false,
 		rowId = "row_" + callCount;
+
 	$('#add-call').submit(function (event) {
 		event.preventDefault();
 		name = $('input.name').val() || '';
 		number = $('input.number').val() || '';
 		time = $('input.time').val() || '';
 		checkName(name);
-		checkNumber(number);
-		number = checkNumber(number);
+		number = checkNumber(number); // you can assign function result directly to variable number
 		checkTime(time);
 		addToLocalStorage(name, number, time);
 	});
@@ -27,16 +27,22 @@ $(document).ready(function () {
 	};
 
 	function checkNumber(number) {
+		// phone number is to long you can write it 2 time shorter
 		if ((/^([0]{2}[\s]?[(]?[0-9]{3}[)]?[\s]?[0-9]{3}[-\s]?[0-9]{3}[-\s]?[0-9]{3})|([+][\s]?[(]?[0-9]{3}[)]?[\s]?[0-9]{3}[-\s]?[0-9]{3}[-\s]?[0-9]{3})$/i).test(number)) {
 			var numberArr;
 			numberValid = true;
 			inputIsValid('number');
 			number = changeSymbol(number, '+', '00');
+
+			// you can write three lines below in single line; becouse replace support regular expr, so write expression for this symbols "(", ")" and "-";
 			number = changeSymbol(number, '(', '');
 			number = changeSymbol(number, ')', '');
 			number = changeSymbol(number, '-', '');
+
 			numberArr = number.split('');
-			addSpace(numberArr, 2);
+
+			// you should parse you number in one helper function;
+			addSpace(numberArr, 2); 
 			addSpace(numberArr, 6);
 			addSpace(numberArr, 10);
 			addSpace(numberArr, 14);
@@ -61,6 +67,7 @@ $(document).ready(function () {
 	}
 
 	function checkTime(time) {
+		// pattern can be more simply find in google js regular expression for time 24 hours
 		if ((/^([0]{1}\d[:]{1}[0-5]{1}\d)|([1]{1}\d[:]{1}[0-5]{1}\d)|([2]{1}[0-4]{1}[:]{1}[0-5]{1}\d)$/i).test(time)) {
 			timeValid = true;
 			inputIsValid('time');
@@ -87,7 +94,7 @@ $(document).ready(function () {
 			$('input.name').focus();
 			callsTimeCheck();
 			rowId = "row_" + callCount;
-			var callsObj = {
+			var callsObj = { // all vars should be on the top position;
 				rowId: {
 					'name': name,
 					'number': number,
@@ -119,6 +126,7 @@ $(document).ready(function () {
 			$('input.' + type + '+.error').removeClass('active');
 		}
 	};
+	// please move all logick 130-144 lines after helpers, for example 230 line;
 	grid = $('table.calls')[0];
 
 	grid.onclick = function (e) {
@@ -136,7 +144,7 @@ $(document).ready(function () {
 	};
 
 	function sortGrid(colNum, type) {
-		var tbody = $('table.calls tbody')[0];
+		var tbody = $('table.calls tbody')[0]; // use one var and one style for you code;
 		var rowsArray = [].slice.call(tbody.rows);
 		var compare;
 		switch (type) {
@@ -149,6 +157,7 @@ $(document).ready(function () {
 		case 'number sort':
 			$('th.time').removeClass('sort');
 			compare = function (rowA, rowB) {
+				// what the difference with code on 154 line? you can write helper func and return reverted result on this case;
 				return rowA.cells[colNum].innerHTML.slice(0, 2) + rowA.cells[colNum].innerHTML.slice(3) > rowB.cells[colNum].innerHTML.slice(0, 2) + rowB.cells[colNum].innerHTML.slice(3) ? -1 : 1;
 			};
 			break;
@@ -161,6 +170,7 @@ $(document).ready(function () {
 		case 'string sort':
 			$('th.name.sort').removeClass('sort');
 			compare = function (rowA, rowB) {
+				// same problem like in 160 line;
 				return rowA.cells[colNum].innerHTML > rowB.cells[colNum].innerHTML ? -1 : 1;
 			};
 			break;
@@ -183,15 +193,18 @@ $(document).ready(function () {
 			callsTimeCheck();
 		});
 	};
-
+	// this function is to complicated divedi it by parts
 	function callsTimeCheck() {
-		var min;
+		var min; // you should you single variable;
 		var date = new Date(),
 			dateHours = date.getHours(),
 			dateMinutes = date.getMinutes(),
-			arr = []
-		$('.calls td.time').each(function (index, el) {
+			arr = [];
+
+		$('.calls td.time').each(function (index, el) { // for now app doesn't work because we don't have any $('.calls td.time')
+
 			var hoursCheck = $(this).text().slice(0, 2) < dateHours;
+
 			if (hoursCheck || (hoursCheck && $(this).text().slice(3) < dateMinutes)) {
 				$(this).closest('tr').find('input').attr('checked', 'checked');
 				arr[arr.length] = $(this).text().slice(0, 2) + $(this).text().slice(3);
@@ -206,7 +219,7 @@ $(document).ready(function () {
 		}
 
 		min = min.slice(0, 2) + ":" + min.slice(2);
-		$('.calls td.time').each(function (index, el) {
+		$('.calls td.time').each(function (index, el) { // same problem like in 204 line;
 			if ($(this).text() == min) {
 				$('.next-call table tr').remove();
 				$(this).closest('tr').clone().appendTo('.next-call table');
@@ -214,6 +227,7 @@ $(document).ready(function () {
 			}
 		})
 	};
+
 	paseLocalStorage();
 	removeRow();
 	callsTimeCheck();
@@ -222,6 +236,7 @@ $(document).ready(function () {
 	}, 60000);
 
 	$('.all, .next, .finished').click(function (event) {
+		// move code below to helper with name what happen below;
 		event.preventDefault();
 		$('tr').removeClass('hidden');
 		if ($(this)[0] == $('.next')[0]) {
